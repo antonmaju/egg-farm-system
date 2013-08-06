613f6be0ce188c169732ee42e63499c5832517ef
Anton Setiawan <antonmaju@gmail.com>
2013-08-06 21:56:36 +0700

    Add EggProduction model

---
+++ src/EggFarmSystem/EggFarmSystem/Models/EggProduction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EggFarmSystem.Models
+{
+    public class EggProduction :Entity
+    {
+        public EggProduction()
+        {
+            Details = new List<EggProductionDetail>();
+        }
+
+        public DateTime Date { get; set; }
+
+        public List<EggProductionDetail> Details { get; set; } 
+    }
+
+    public class EggProductionDetail
+    {
+        public Guid HouseId { get; set; }
+
+        public int GoodEggCount { get; set; }
+
+        public decimal RetailQuantity { get; set; } //in kg
+
+        public decimal Fcr { get; set; }
+
+        public int CrackedEggCount { get; set; }
+    }
+}
