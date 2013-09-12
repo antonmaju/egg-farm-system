4c5d22cdf4f920571fd37b4d988567c5bdf3e72d
Anton Setiawan <antonmaju@gmail.com>
2013-09-12 20:57:51 +0700

    update hen depreciation service

---
+++ src/EggFarmSystem/EggFarmSystem.Client/Core/Services/HenDepreciationServiceClient.cs
@@ -0,0 +1,46 @@
+﻿using EggFarmSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EggFarmSystem.Client.Core.Services
+{
+    public class HenDepreciationServiceClient : ServiceClient, IHenDepreciationService
+    {
+        public HenDepreciationServiceClient(IClientContext context) : base(context)
+        {
+
+        }
+
+        protected override string ResourceUrl
+        {
+            get { return "hendepreciation"; }
+        }
+
+        public Models.SearchResult<Models.HenDepreciation> Search(Models.DateRangeSearchInfo searchInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.HenDepreciation Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.HenDepreciation GetByDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Models.HenDepreciation depreciation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/EggFarmSystem/EggFarmSystem.Core.Tests/Services/HenDepreciationServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using EggFarmSystem.Services;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EggFarmSystem.Core.Tests.Services
+{
+    public class HenDepreciationServiceTests : IDisposable
+    {
+        private IHenDepreciationService service;
+        private IDbConnectionFactory factory;
+
+        public HenDepreciationServiceTests()
+        {
+            factory = DatabaseTestInitializer.GetConnectionFactory();
+            service = new HenDepreciationService(factory);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
