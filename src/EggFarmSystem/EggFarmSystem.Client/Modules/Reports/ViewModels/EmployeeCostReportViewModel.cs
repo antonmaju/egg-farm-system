d2f2e9e583db9fa48b07cc9395eef0f52a75eae3
Anton Setiawan <antonmaju@gmail.com>
2013-10-17 22:41:12 +0700

    add date range selector

--- src/EggFarmSystem/EggFarmSystem.Client/Modules/Reports/ViewModels/EmployeeCostReportViewModel.cs
+++ src/EggFarmSystem/EggFarmSystem.Client/Modules/Reports/ViewModels/EmployeeCostReportViewModel.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EggFarmSystem.Client.Commands;
 using EggFarmSystem.Client.Core;
+using EggFarmSystem.Resources;
 using MigraDoc.DocumentObjectModel;
 
 namespace EggFarmSystem.Client.Modules.Reports.ViewModels
 {
     public class EmployeeCostReportViewModel : ViewModelBase
     {
-        private DateTime startDate;
-        private DateTime endDate;
+        private DateTime startDate=DateTime.Today;
+        private DateTime endDate=DateTime.Today;
         private Document document;
+
+        public DelegateCommand ViewCommand;
+
+        public EmployeeCostReportViewModel()
+        {
+            InitializeCommands();
+
+        }
+
+        #region properties
 
         public DateTime StartDate
         {
@@ -34,5 +46,40 @@ namespace EggFarmSystem.Client.Modules.Reports.ViewModels
                 OnPropertyChanged("Document");
             }
         }
+
+        public IList<CommandBase> NavigationCommands { get; private set; }
+
+        #endregion
+
+        #region commands
+
+        private void InitializeCommands()
+        {
+            ViewCommand = new DelegateCommand(ViewReport, CanView){Text = ()=> LanguageData.General_View};
+            NavigationCommands = new List<CommandBase>(){ViewCommand};
+        }
+
+        bool CanView(object param)
+        {
+            return EndDate <= StartDate;
+        }
+
+        void ViewReport(object param)
+        {
+            var document = new Document();
+            document.UseCmykColor = true;
+            var section = document.AddSection();
+            var paragraph = section.AddParagraph();
+            
+            paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
+            paragraph.AddFormattedText("Employee Cost Report", TextFormat.Bold);
+
+            Document = document;
+        }
+
+        #endregion
+
+       
+
     }
 }
