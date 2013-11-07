78e91ce52e0007a06bcd710e7f60c825550ec15d
finestrike <finestrike@yahoo.com>
2013-11-07 22:14:22 +0700

    fix usage report using service

--- src/EggFarmSystem/EggFarmSystem.Client/Core/Services/ReportingServiceClient.cs
+++ src/EggFarmSystem/EggFarmSystem.Client/Core/Services/ReportingServiceClient.cs
@@ -30,7 +30,7 @@ namespace EggFarmSystem.Client.Core.Services
         public IList<UsageSummary> GetUsageSummary(DateTime start, DateTime end)
         {
             string url = string.Format("{0}/usagesummary/{1}/{2}",
-               ResourceUrl, start.ToString("YYYY-MM-dd"), end.ToString("YYYY-MM-dd"));
+               ResourceUrl, start.ToString("yyyy-MM-dd"), end.ToString("yyyy-MM-dd"));
 
             return CreateGetRequest<List<UsageSummary>>(Guid.Empty, url);
 		}
